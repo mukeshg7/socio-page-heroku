194d4a0f24df9acd8012d81bcdbff7616fb99cb4
Mukesh Gupta <mukeshgupta202000@gmail.com>
2020-07-22 13:56:32 +0530

    deploy3

--- app.js
+++ app.js
@@ -364,9 +364,17 @@ app.post('/signup', async (req, res) => {
 
 
 
-app.use(express.static('client/build'));
 
-app.use(function(req, res) {
-	res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
